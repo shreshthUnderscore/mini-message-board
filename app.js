e1db636a0da209aaddb5b79069359a27acb40ad8
shreshthUnderscore <88784050+shreshthUnderscore@users.noreply.github.com>
2024-12-13 10:26:16 +0000

    feat: add the ability to change the port number to allow it to deploy

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.set("view engine", "ejs");
 app.use("/", indexRouter);
 app.use("/new", newMessage);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`port running at ${PORT}`);
